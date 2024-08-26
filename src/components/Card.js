a6940d5d8fe056bbff772d25173fd78fd0ed0c16
GorkyAnge <gorkyange@gmail.com>
2024-08-25 19:41:43 -0500

    Format Prettier

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,26 @@
-import Image from 'next/image';
-import Mark from './Mark';
+import Image from "next/image";
+import Mark from "./Mark";
 const Card = () => {
-    return ( 
-        <div className='bg-white drop-shadow-md p-[16px] w-[368px] space-y-4 rounded-xl'>
-            <Image className="rounded-xl" src="/images/cactus_img.jpg" width={336} height={152} alt="Cactus" />
-            <Mark/>
-            <h1 className='text-lg font-semibold'>Embracing Minimalism</h1>
-            <p className='text-sm font-light'>From minimalist sculptures to minimalist paintings, this blog will inspire you to appreciate the beauty that lies in simplicity.</p>
-            <div className='p-[4px] border-t-[#6C727F]     bg-slate-300'>
-                <p className='text-xs font-light'>Annie Spratt</p>
+  return (
+    <div className="bg-white drop-shadow-md p-[16px] w-[368px] space-y-4 rounded-xl">
+      <Image
+        className="rounded-xl"
+        src="/images/cactus_img.jpg"
+        width={336}
+        height={152}
+        alt="Cactus"
+      />
+      <Mark />
+      <h1 className="text-lg font-semibold">Embracing Minimalism</h1>
+      <p className="text-sm font-light">
+        From minimalist sculptures to minimalist paintings, this blog will
+        inspire you to appreciate the beauty that lies in simplicity.
+      </p>
+      <div className="p-[4px] border-t-[#6C727F]     bg-slate-300">
+        <p className="text-xs font-light">Annie Spratt</p>
+      </div>
+    </div>
+  );
+};
 
-            </div>
-            
-
-        </div>
-     );
-}
- 
 export default Card;
--- src/components/Mark.js
+++ src/components/Mark.js
@@ -1,9 +1,9 @@
 const Mark = () => {
-    return ( 
-        <div className="inline-block py-1.5 px-4 bg-[#E6D6FC] rounded-full">
-            <p className="text-sm/[10px] text-[#883AE1] font-medium">Design</p>
-        </div>
-     );
-}
- 
+  return (
+    <div className="inline-block py-1.5 px-4 bg-[#E6D6FC] rounded-full">
+      <p className="text-sm/[10px] text-[#883AE1] font-medium">Design</p>
+    </div>
+  );
+};
+
 export default Mark;
